50717e545ce5fbb049e1a72bb4260c5258cb593f
Steve Baker <steve_m_baker@hotmail.com>
2020-04-07 09:44:16 +0100

    Added some validation Added undo function

--- scripts/StopSleepWakers.ps1
+++ scripts/StopSleepWakers.ps1
@@ -4,9 +4,13 @@
 function Get-WakeArmedDevices {
     BEGIN {
         $wakearmeddevices = powercfg.exe -devicequery wake_armed
-        # Query always adds a blank entry at the end - this removes it
-        $entrytoremove = ($wakearmeddevices.count) - 1
-        $wakearmeddevices[$entrytoremove] = $null
+        if ($wakearmeddevices -eq "NONE") {
+            throw "No devices are wake-armed!" # could this be done better?
+        } else {
+            # Query always adds a blank entry at the end - this removes it
+            $entrytoremove = ($wakearmeddevices.count) - 1
+            $wakearmeddevices[$entrytoremove] = $null
+        } #if
     }
 
     PROCESS {
@@ -56,25 +60,44 @@ function Stop-SleepWakers {
 
         foreach ($wakearmeddevice in $wakearmeddevices) {
             if ($PSCmdlet.ShouldProcess("Device: [$wakearmeddevice]", "Disabling Wake from Sleep")) {
+                Write-Verbose "Disabling [$wakearmeddevice]..."
                 powercfg.exe -devicedisablewake "$wakearmeddevice"
+                $wakearmeddevice | Out-File $logfilename -Append
             } #shouldprocess
         } #foreach
     } #process
 
     END {
-        notepad $logfilename
+        Write-Verbose "All specified devices prevented from waking the computer from sleep."
+        Write-Verbose "If you wish to revert this operation, the log file path is: [$logfilename]"
+        Write-Verbose "Do so by using the command:"
+        Write-Verbose "Undo-DisableSleepWakers -LogFilePath $logfilename"
     }
 
 } #function
 
 function Undo-DisableSleepWakers {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     param (
+        [ValidateScript( {
+                if (-Not ($_ | Test-Path) ) {
+                    throw "File or folder does not exist"
+                }
+                if ($_ -notmatch "(\.txt)") {
+                    throw "The file specified in the path argument must be of type txt "
+                }
+                return $true
+            })]
         [Parameter(Mandatory = $true)]
         [string]$LogFilePath
     )
 
-    # Need something to validate log file, fail if not there, import log to var, supportsshouldprocess to allow user
-    # to allow/deny devices to be re-enabled
+    $wakedisableddevices = Get-Content -Path $LogFilePath
 
+    foreach ($wakedisableddevice in $wakedisableddevices) {
+        if ($PSCmdlet.ShouldProcess("Device: [$wakedisableddevice]", "Enabling Wake from Sleep")) {
+            Write-Verbose "Re-enabling [$wakedisableddevice]..."
+            powercfg -deviceenablewake "$wakedisableddevice"
+        } #shouldprocess
+    } #foreach
 } #function
