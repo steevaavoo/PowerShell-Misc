cced9e09856185243465934368d93ec83651297b
Steve Baker <steve_m_baker@hotmail.com>
2020-09-29 12:59:23 +0100

    Added some snippets

---
+++ scripts/Add-MultipleMailboxPermission.ps1
@@ -0,0 +1,17 @@
+$mailboxname =
+$displayname =
+$($newmailbox.alias) =
+$secretary =
+
+$newmailboxes = Import-Csv -path ~\desktop\newmailboxes.csv
+$newmailboxes.DisplayName
+
+foreach ( $newmailbox in $newmailboxes ) {
+    Add-RecipientPermission -Identity $newmailbox.alias -AccessRights SendAs -Trustee $newmailbox.secretary
+    Add-MailboxPermission -Identity $newmailbox.alias -User $newmailbox.secretary -AccessRights FullAccess -InheritanceType All
+}
+
+
+$newmailboxes[0].secretary
+
+Add-RecipientPermission -Identity $newmailbox.alias -AccessRights SendAs -Trustee $newmailbox.secretary
