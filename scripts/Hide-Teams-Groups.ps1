256d7fa89607887c30b46aebca6fb94d2f21bd9f
Steve Baker <steve_m_baker@hotmail.com>
2020-12-07 13:23:58 +0000

    Added hide teams groups script

--- scripts/Hide-Teams-Groups.ps1
+++ scripts/Hide-Teams-Groups.ps1
@@ -1,4 +1,17 @@
+# Getting all Teams Groups into a variable
 $groups = Get-UnifiedGroup
 
-$groups | Select-Object DisplayName, Alias, HiddenFromExchangeClientsEnabled | Sort-Object HiddenFromExchangeClientsEnabled
+# Isolating just the groups which are visible in the Global Address List and Navigation Panes in Outlook
+$visiblegroups = $groups | Where-Object HiddenFromExchangeClientsEnabled -like '*false*'
 
+# Confirming the results (all should show 'False' in HiddenFromExchangeClientsEnabled)
+$visiblegroups | Select-Object alias, HiddenFromExchangeClientsEnabled
+
+# Changing all the visible groups to be invisible
+foreach ($visiblegroup in $visiblegroups ) {
+    Set-UnifiedGroup -Identity $visiblegroup.identity -HiddenFromExchangeClientsEnabled
+}
+
+# Checking the status of the groups after the change
+$groups = Get-UnifiedGroup
+$groups | Select-Object Alias, HiddenFromExchangeClientsEnabled
