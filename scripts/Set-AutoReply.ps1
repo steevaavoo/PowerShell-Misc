ac5e23a3b7053f19ec31ebb3ff00fc9a8a63a0a7
Steve Baker <steve_m_baker@hotmail.com>
2020-12-22 11:51:54 +0000

    Added refinements

--- scripts/Set-AutoReply.ps1
+++ scripts/Set-AutoReply.ps1
@@ -4,13 +4,17 @@ Connect-sbExopsSession
 # Getting List of Mailbox Aliases
 Get-Mailbox | Select-Object Alias
 
-# Getting the autoreply from a file into a variable (HTML format, single line)
+# Filtering for specific mailboxes into a variable for use later
+$identities = Get-Mailbox | Where-Object Alias -like "*example*" | Select-Object -ExpandProperty Alias
+
+# Getting the autoreply from a file into a variable (HTML format, single line, or plain text)
 $ooo = (Get-Content -Path ~\desktop\ooo.txt)
+$ooo = "The office is now closed until [insert date and time]."
 
 $identities = "person.1", "person.2", "person.3"
 # US date format required
-$starttime = "11/19/2020 12:00 PM"
-$endtime = "11/19/2020 4:00 PM"
+$starttime = "12/24/2020 12:00 PM"
+$endtime = "01/04/2021 8:00 AM"
 $autoreplystate = "Scheduled"
 
 Get-MailboxAutoReplyConfiguration -Identity $identity
@@ -18,3 +22,6 @@ Get-MailboxAutoReplyConfiguration -Identity $identity
 foreach ($identity in $identities) {
     Set-MailboxAutoReplyConfiguration -Identity $identity -InternalMessage $ooo -ExternalMessage $ooo -ExternalAudience All -StartTime $starttime -EndTime $endtime -AutoReplyState $autoreplystate
 }
+
+# Disconnecting from EXOL
+Get-PSSession | Remove-PSSession
