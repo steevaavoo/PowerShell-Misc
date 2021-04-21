016c5507b004de73aad1a7d0da440443dd770ce0
Steve Baker <steve_m_baker@hotmail.com>
2021-04-21 10:45:47 +0100

    Added some refinements

--- scripts/LinkRemoteMailbox.ps1
+++ scripts/LinkRemoteMailbox.ps1
@@ -1,5 +1,5 @@
 # Run the below to add Exchange Management Tools to standard PowerShell Terminal - NOTE only run on Exchange server
-# This, along with Connect-ExopsSession would allow this entire process to be run in one session.
+# This, along with Connect-ExchangeOnline would allow this entire process to be run in one session.
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 Import-Module ActiveDirectory
 
@@ -7,21 +7,21 @@ Import-Module ActiveDirectory
 Connect-ExchangeOnline
 Connect-MsolService
 
-
-
-# Prep (Phase 0)
+# Prep (Phase 0) - correct the placeholders below before running
 
 $userprincipalname = 'user.name@domain.com'
 $mailbox = Get-Mailbox -Identity $userprincipalname
 $msoluser = Get-MsolUser -UserPrincipalName $userprincipalname
+# companyinitialname Example "contosocouk" - the .onmicrosoft.com is automatically appended below
 $companyinitialname = "initialnameoftenant"
 $remoteroutingaddress = "$companyinitialname.onmicrosoft.com"
 
-# Filtering out the Company Initial Name-based addresses - use the new variable when creating the local AD user
+# Filtering out the Company Initial Name-based addresses - the $adproxyaddresses variable will be used when creating the local AD user - the correct SMTP address for a successful match should already be in place as a result of this
 $adproxyaddresses = $msoluser.ProxyAddresses | Where-Object { $_ -notlike '*companyinitialname*' }
 
-# Phase 1 - do soon after AAD Sync completes
+# Phase 1 - do soon after AAD Sync completes, to allow time for troubleshooting/checks
 
+# Be sure to change the Path below to the appropriate OU
 $aduserparams = @{
     Name              = $($msoluser.DisplayName);
     SamAccountName    = "$($msoluser.FirstName).$($msoluser.LastName)";
@@ -41,25 +41,34 @@ $aduserparams = @{
     Fax               = $($msoluser.Fax);
     MobilePhone       = $($msoluser.MobilePhone);
     Country           = $($msoluser.Country);
-    OtherAttributes   = @{
-        proxyAddresses = $adproxyaddresses; # This is problematic. Check other script (change proxy addresses) to find out how to make it work.
-    }
 }
+
 
 # Create user with above splatted parameters
 New-AdUser @aduserparams -WhatIf
 
-# Add user to standard groups
+# Set AD user's Email Addresses to match those extracted from Exchange Online
+Set-ADUser $aduserparams.samAccountName -replace @{proxyaddresses = $adproxyaddresses }
 
-# Phase 2 - do after the mailbox is linked successfully
+# Add user to standard groups (rename placeholders accordingly using shown syntax)
+$groups = 'group1','group2','group3'
+
+foreach($group in $groups){
+    Add-ADGroupMember -Identity $group -Members $aduserparams.samAccountName
+}
+
+# Add user to non-standard groups using ADUC.
+
+# Phase 2 - do after the mailbox is linked successfully (after AD Sync)
 
 # Run the below on Exchange Online to get the Remote Mailbox GUID:
-# Connect-sbExopsSession
+# Connect-ExchangeOnline
 Get-Mailbox $userprincipalname | Format-List ExchangeGuid
 $remoteguid = Get-Mailbox $userprincipalname | Select-Object -ExpandProperty ExchangeGuid
-# $remoteguid.Guid | Set-Clipboard
 
-# Run the below on Hybrid Server Exchange Powershell to link the local User to the remote mailbox
+# Run the below on Hybrid Server Exchange Powershell to link the local User to the remote mailbox - this will allow local management of the mailbox
 Enable-RemoteMailbox $userprincipalname -RemoteRoutingAddress $remoteroutingaddress
 
 Set-RemoteMailbox $userprincipalname -ExchangeGuid $remoteguid.Guid
+
+#TODO: Script something to change the emails for all Contraflex and Manuplas users to aisltd.com (the AD ones can be done via Email Address Policy)
